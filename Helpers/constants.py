8776ab6a6363a38e018e15795f44327621682b1d
bahaozsahin <45354173+bahaozsahin@users.noreply.github.com>
2021-02-02 19:21:58 +0300

    Create constants.py

---
+++ Helpers/constants.py
@@ -0,0 +1,18 @@
+class Screen_Constants:
+    SCREEN_WIDTH = 480
+    SCREEN_HEIGHT = 480
+
+    GRID_SIZE = 20
+    GRID_WIDTH = SCREEN_WIDTH / GRID_SIZE
+    GRID_HEIGHT = SCREEN_HEIGHT / GRID_SIZE
+
+    GRID_HEIGHT = int(GRID_HEIGHT)
+    GRID_WIDTH = int(GRID_WIDTH)
+    SCREEN_HEIGHT = int(SCREEN_HEIGHT)
+    SCREEN_WIDTH = int(SCREEN_WIDTH)
+
+
+    UP = (0 , 1)
+    DOWN = (0 , -1)
+    LEFT = (-1, 0)
+    RIGHT = (1 , 0)
