378bb4a2c999ec4594c7673862575ebaaadac84d
Baha <45354173+bahaozsahin@users.noreply.github.com>
2021-02-07 13:47:17 +0300

    Update main.py
    fixed snake drawing problem

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-from Objects.snakey import snake
 from Helpers.constants import Screen_Constants
 from Objects import snakey
 from Objects import food
@@ -9,12 +8,14 @@ def main():
 
 # Setting the clock and the screen mode
     clock = pygame.time.Clock()
-    screen = pygame.display.set_mode((Screen_Constants.SCREEN_WIDTH, Screen_Constants.SCREEN_HEIGHT), 0, 32)
+    screen = pygame.display.set_mode([Screen_Constants.SCREEN_WIDTH, Screen_Constants.SCREEN_HEIGHT])
+    #screen = pygame.display.set_mode((Screen_Constants.SCREEN_WIDTH, Screen_Constants.SCREEN_HEIGHT), 0, 32) ?
 
 # Creating the surface
     surface = pygame.Surface(screen.get_size())
     surface = surface.convert()
     draw_grid(surface)
+
     snake = snakey.snake()
     apple = food.apple()
     myfont = pygame.font.SysFont("monospace", 16)
@@ -30,22 +31,22 @@ def main():
             snake.length += 1
             score += 1
             apple.randomise_position()
-        screen.blit(surface, (0, 0))
-        score_text = myfont.render("Score {0}".format(score), 1 ,(0, 0, 0))
         snake.draw(surface)
         apple.draw(surface)
+        screen.blit(surface, (0, 0))
+        score_text = myfont.render("Score {0}".format(score), 1 ,(0, 0, 0))
         screen.blit(score_text, (5, 10))
         pygame.display.update()
 
 
 def draw_grid(surface):
-        for y in range(Screen_Constants.GRID_HEIGHT):
-            for x in range(Screen_Constants.GRID_WIDTH):
-                if (x + y) % 2 == 0:
-                    rectangles = pygame.Rect((x*Screen_Constants.GRID_SIZE, y*Screen_Constants.GRID_SIZE), (Screen_Constants.GRID_SIZE, Screen_Constants.GRID_SIZE))
-                    pygame.draw.rect(surface, (93, 216, 228), rectangles)
-                else:
-                    rrectangles = pygame.Rect((x*Screen_Constants.GRID_SIZE, y*Screen_Constants.GRID_SIZE), (Screen_Constants.GRID_SIZE, Screen_Constants.GRID_SIZE))
-                    pygame.draw.rect(surface, (84, 194, 205), rrectangles)
+    for y in range(Screen_Constants.GRID_HEIGHT):
+        for x in range(0, int(Screen_Constants.GRID_WIDTH)):
+            if (x + y) % 2 == 0:
+                rectangles = pygame.Rect((x*Screen_Constants.GRID_SIZE, y*Screen_Constants.GRID_SIZE), (Screen_Constants.GRID_SIZE, Screen_Constants.GRID_SIZE))
+                pygame.draw.rect(surface, (93, 216, 228), rectangles)
+            else:
+                rrectangles = pygame.Rect((x*Screen_Constants.GRID_SIZE, y*Screen_Constants.GRID_SIZE), (Screen_Constants.GRID_SIZE, Screen_Constants.GRID_SIZE))
+                pygame.draw.rect(surface, (84, 194, 205), rrectangles)
 
 main()
